cd4b581f57535a31a813cc8c939c7b8153b3de1d
meghamohan <meghamohan1989@gmail.com>
2017-05-01 21:47:45 +0000

    adding task6

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-print('Welcome to {:s} '.format(str1) + '{:s}!'.format(str2))
+str1 = str1 + ' ' + str2
+print('Welcome to {}!'.format(str1))
