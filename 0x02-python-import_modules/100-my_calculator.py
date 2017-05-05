d3061821d2e933bf4e616737f6fd1a99cfb73658
meghamohan <meghamohan1989@gmail.com>
2017-05-05 03:58:15 +0000

    made pep3 compatible

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,22 +2,29 @@
 def main():
     from calculator_1 import add, sub, mul, div
     import sys
-    if(len(sys.argv) != 4):
+
+    if (len(sys.argv) != 4):
         print("./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
+    a = int(sys.argv[1])
     op = sys.argv[2]
-    if (op != '+' or op != '-' or op != '*' or op != '/'):
+    b = int(sys.argv[3])
+
+    if op not in "+-*/":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    if (op == '+'):
-        res = add(int(sys.argv[1]), int(sys.argv[3]))
-    if (op == '-'):
-        res = sub(int(sys.argv[1]), int(sys.argv[3]))
-    if (op == '*'):
-        res = mul(int(sys.argv[1]), int(sys.argv[3]))
-    if (op == '/'):
-        res = div(int(sys.argv[1]), int(sys.argv[3]))
-    print("{} {} {} = {}".format(sys.argv[1], op, sys.argv[3], res))
 
-    if(__name__ == "__main__"):
-        main()
+    if (op == '+'):
+        res = add(a, b)
+    if (op == '-'):
+        res = sub(a, b)
+    if (op == '*'):
+        res = mul(a, b)
+    if (op == '/'):
+        res = div(a, b)
+
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, res))
+
+if(__name__ == "__main__"):
+    main()
