e6699b71905e16eb35399941075823ebc180f4d1
meghamohan <meghamohan1989@gmail.com>
2017-05-09 20:47:38 +0000

    added edge case

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    size = len(my_list)
-    for i in range(size - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+    if (my_list is not None and len(my_list) > 0):
+        size = len(my_list)
+        for i in range(size - 1, -1, -1):
+            print("{:d}".format(my_list[i]))
