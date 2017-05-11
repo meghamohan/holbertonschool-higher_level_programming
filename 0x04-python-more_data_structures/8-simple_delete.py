9c5afe1d96871b474a5b03e65b740c7c1e5c6907
meghamohan <meghamohan1989@gmail.com>
2017-05-11 23:38:37 +0000

    chnaged for loop

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(my_dict, key=""):
     dictCopy = dict(my_dict)
-    if key in dictCopy:
+    if key in dictCopy.keys():
         del dictCopy[key]
     return dictCopy
