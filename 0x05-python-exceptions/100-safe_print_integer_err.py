365809cfb42544d772bf8a183aaf22efe19dd97a
meghamohan <meghamohan1989@gmail.com>
2017-05-12 23:26:16 +0000

    chnaged error types

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
-    import sys
     try:
         print("{:d}".format(value))
     except Exception as err:
-        print(err)
         sys.stderr.write("Exception: " + str(err) + "\n")
         return (False)
     return (True)
