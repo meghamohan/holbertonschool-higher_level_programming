f773e7c18ed8c7e429ae8456ab7ccdb7652dfc70
meghamohan <meghamohan1989@gmail.com>
2017-05-12 19:33:51 +0000

    return value changed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if my_list is None or my_list == [] or x == 0:
+        print()
+        return (0)
     try:
         i = 0
         for i in range(x):
