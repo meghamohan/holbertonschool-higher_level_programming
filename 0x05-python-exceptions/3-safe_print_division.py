0c7ff2cd860613ee84c7cce739016dd7f73cbd35
meghamohan <meghamohan1989@gmail.com>
2017-05-12 22:12:45 +0000

    adding task3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,7 @@ def safe_print_division(a, b):
     try:
         div = a / b
     except ArithmeticError:
-        div ='None'
+        div = 'None'
     finally:
         print("Inside result: {}".format(div))
     return div
-        
