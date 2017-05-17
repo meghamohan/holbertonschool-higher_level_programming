502b1562f3866501c363ccfde3768bd4b89f735a
meghamohan <meghamohan1989@gmail.com>
2017-05-17 17:50:32 +0000

    pep8 correction

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
-
 """
 a and b must be integers or floats, otherwise raise a TypeError
 exception with the message 'a must be an integer' or 'b must be an integer'
 should be raised
 """
+
+
 def add_integer(a, b):
         if isinstance(a, (int, float)) is not True:
             raise TypeError('a must be an integer')
