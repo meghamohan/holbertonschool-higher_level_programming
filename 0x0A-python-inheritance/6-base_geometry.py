ff9b151a3a9507556756f20d3ff2c0a4013739df
meghamohan <meghamohan1989@gmail.com>
2017-05-30 21:49:46 +0000

    adding task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""this is a class that has area function that raises an exception"""
+
+
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
