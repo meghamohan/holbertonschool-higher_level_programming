c1557d111df16ba0e73ab6c037a721d3e568daa4
meghamohan <meghamohan1989@gmail.com>
2017-06-03 01:08:29 +0000

    fixing issues

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,14 +10,15 @@ class Rectangle:
         Rectangle.number_of_instances += 1
 
     def __str__(self):
-        strng = (('#' * self.width) + '\n') * self.height
-        return strng[:-1]
+        string = (('#' * self.__width) + '\n') * self.__height
+        return string[:-1]
 
     def __repr__(self):
         return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
 
     def __del__(self):
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
@@ -46,9 +47,9 @@ class Rectangle:
             self.__height = value
 
     def area(self):
-        return (self.width * self.height)
+        return (self.__width * self.__height)
 
     def perimeter(self):
-        if (self.width == 0 or self.height == 0):
+        if (self.__width == 0 or self.__height == 0):
             return 0
-        return 2 * (self.width + self.height)
+        return 2 * (self.__width + self.__height)
