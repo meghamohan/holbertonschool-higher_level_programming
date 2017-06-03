0c655cc35d489bdbe7227aeaafe101bf2d76dab0
meghamohan <meghamohan1989@gmail.com>
2017-06-03 23:17:41 +0000

    adding task12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Student class """
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            myDict = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    myDict[i] = getattr(self, i)
+            return myDict
