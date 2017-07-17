231d7cabdfb3f43f3b2c7d44bcb932e51098657e
meghamohan <meghamohan1989@gmail.com>
2017-07-17 18:59:02 +0000

    coorected field name

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,4 +3,4 @@ SELECT tv_genres.name FROM tv_genres WHERE tv_genres.id NOT IN
 	(SELECT tv_show_genres.genre_id FROM tv_show_genres
 	INNER JOIN tv_shows
 	ON tv_shows.id = tv_show_genres.show_id
-	WHERE tv_shows.name = "Dexter") ORDER BY name ASC;
+	WHERE tv_shows.title = "Dexter") ORDER BY name ASC;
