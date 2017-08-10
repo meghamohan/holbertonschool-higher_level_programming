4b23dc573d14d1559cfd0a7712d5d997cd1b7515
meghamohan <meghamohan1989@gmail.com>
2017-08-10 23:45:25 +0000

    pep8 fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,8 +5,6 @@ this script lists all states from the database hbtn_0e_0_usa
 if __name__ == "__main__":
     import sys
     import MySQLdb
-
-
     usr = sys.argv[1]
     pswd = sys.argv[2]
     database = sys.argv[3]
