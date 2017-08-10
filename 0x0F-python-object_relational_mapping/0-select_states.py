e8fc9e55130e64fd803485ca1e450f4f45d4255a
meghamohan <meghamohan1989@gmail.com>
2017-08-10 20:09:32 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+this script lists all states from the database hbtn_0e_0_usa
+"""
+
+
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        usr = sys.argv[1]
+        pswd = sys.argv[2]
+        database = sys.argv[3]
+        dbConnection = MySQLdb.connect(user=usr, passwd=pswd, db=database)
+        cursr = dbConnection.cursor()
+        cursr.execute("SELECT * FROM states")
+        states = cursr.fetchall()
+        for state in states:
+            print(state)
+        cursr.close()
+        dbConnection.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,5 @@
+This project is about Databases and Python!
+
+The first part is about using the module MySQLdb to connect to a MySQL database and execute your SQL queries.
+
+In the second part, you will use the module SQLAlchemy, an Object Relational Mapper (ORM).
