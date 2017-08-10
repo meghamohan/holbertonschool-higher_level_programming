8514ffb856c10977dfdc1a17ff10c2aa85069656
meghamohan <meghamohan1989@gmail.com>
2017-08-10 20:18:36 +0000

    made some tweaks with import; not sure why the red output checks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,9 +4,9 @@ this script lists all states from the database hbtn_0e_0_usa
 """
 
 
-import sys
-import MySQLdb
 if __name__ == "__main__":
+    import sys
+    import MySQLdb
     if len(sys.argv) == 4:
         usr = sys.argv[1]
         pswd = sys.argv[2]
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         dbConnection = MySQLdb.connect(host="localhost", port=3306,
                                        user=usr, passwd=pswd, db=database)
         cursr = dbConnection.cursor()
-        cursr.execute("SELECT * FROM states")
+        cursr.execute("SELECT * FROM states ORDER BY states.id ASC")
         states = cursr.fetchall()
         for state in states:
             print(state)
