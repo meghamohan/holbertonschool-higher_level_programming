6006db8ad380c995f91d077570a699f5734ecb3e
meghamohan <meghamohan1989@gmail.com>
2017-08-10 20:26:31 +0000

    removed argument validation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,16 +7,15 @@ this script lists all states from the database hbtn_0e_0_usa
 if __name__ == "__main__":
     import sys
     import MySQLdb
-    if len(sys.argv) == 4:
-        usr = sys.argv[1]
-        pswd = sys.argv[2]
-        database = sys.argv[3]
-        dbConnection = MySQLdb.connect(host="localhost", port=3306,
-                                       user=usr, passwd=pswd, db=database)
-        cursr = dbConnection.cursor()
-        cursr.execute("SELECT * FROM states ORDER BY states.id ASC")
-        states = cursr.fetchall()
-        for state in states:
-            print(state)
-        cursr.close()
-        dbConnection.close()
+    usr = sys.argv[1]
+    pswd = sys.argv[2]
+    database = sys.argv[3]
+    dbConnection = MySQLdb.connect(host="localhost", port=3306,
+                                   user=usr, passwd=pswd, db=database)
+    cursr = dbConnection.cursor()
+    cursr.execute("SELECT * FROM states ORDER BY states.id ASC")
+    states = cursr.fetchall()
+    for state in states:
+        print(state)
+    cursr.close()
+    dbConnection.close()
