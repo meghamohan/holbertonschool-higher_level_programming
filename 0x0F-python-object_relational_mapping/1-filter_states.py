b51ec397b97ef20772606944ba0d6c762ce2c93b
meghamohan <meghamohan1989@gmail.com>
2017-08-10 23:58:36 +0000

    adding task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+lists all states with a name starting with N
+ (upper N) from the database hbtn_0e_0_usa
 """
 if __name__ == "__main__":
     import MySQLdb
@@ -8,9 +9,14 @@ if __name__ == "__main__":
     usr = sys.argv[1]
     pswd = sys.argv[2]
     database = sys.argv[3]
-    dbConnection = MySQLdb.connect(host="localhost", port=3306, user=usr, passwd=pswd, db=database)
+    dbConnection = MySQLdb.connect(host="localhost",
+                                   port=3306,
+                                   user=usr,
+                                   passwd=pswd,
+                                   db=database)
     crsr = dbConnection.cursor()
-    crsr.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    crsr.execute("SELECT * FROM states WHERE name
+                 LIKE 'N%' ORDER BY states.id ASC")
     states = crsr.fetchall()
     if states:
         for state in states:
