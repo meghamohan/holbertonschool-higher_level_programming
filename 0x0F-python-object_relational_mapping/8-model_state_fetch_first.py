19015ceec637f45e242175c5d8e98bd47221bad1
meghamohan <meghamohan1989@gmail.com>
2017-08-11 23:11:18 +0000

    took care of edge cases

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -25,5 +25,8 @@ if __name__ == "__main__":
     session = Session()
 
     states = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(states.id, states.name))
+    if states:
+        print("{}: {}".format(states.id, states.name))
+    else:
+        print("Nothing")
     session.close()
