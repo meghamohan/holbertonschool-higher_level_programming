17029260aa8f27d2f4008fafba553bb059e4a321
meghamohan <meghamohan1989@gmail.com>
2017-08-11 00:04:08 +0000

    adding task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
                                    passwd=pswd,
                                    db=database)
     crsr = dbConnection.cursor()
-    crsr.execute("SELECT * FROM states WHERE name
-                 LIKE 'N%' ORDER BY states.id ASC")
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    crsr.execute(query)
     states = crsr.fetchall()
     if states:
         for state in states:
