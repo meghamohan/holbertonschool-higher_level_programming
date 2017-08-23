182a146b51b9e7790f4c7f5ccef7620f24f750c6
meghamohan <meghamohan1989@gmail.com>
2017-08-23 03:26:35 +0000

    line count was more than 3!

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-# curl -sI "$1" | grep "Content-Length" | cut -d: -f2 | cut -d ' ' -f2
-curl -sI "$1" | grep "Content-Length" | rev | cut -d ' ' -f 1 | rev
+curl -sI "$1" | grep "Content-Length" | cut -d: -f2 | cut -d ' ' -f2
