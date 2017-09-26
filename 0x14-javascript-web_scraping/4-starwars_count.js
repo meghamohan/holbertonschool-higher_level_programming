2f5b484121ca6514e2aaefaea60de74b3cb343ed
meghamohan <meghamohan1989@gmail.com>
2017-09-26 21:57:08 +0000

    adding task4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+let count = 0;
+const searchUrl = 'https://swapi.co/api/people/18/';
+const params = {
+  url: process.argv[2],
+  method: 'GET'
+};
+request(params, function (error, response, body) {
+  if (!error) {
+    const jsonObj = JSON.parse(body);
+    for (let i of jsonObj.results) {
+      for (let j of i.characters) {
+        if (j.indexOf(searchUrl) >= 0) {
+          count++;
+        }
+      }
+    }
+  }
+  console.log(count);
+});
