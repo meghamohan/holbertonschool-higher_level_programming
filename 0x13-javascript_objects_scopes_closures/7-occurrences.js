ffd53e016f4dfbb2502d38823de80badc26e3a59
meghamohan <meghamohan1989@gmail.com>
2017-09-26 05:51:31 +0000

    semistnd fix

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, search_element) {
+exports.nbOccurences = function (list, searchElement) {
   let c = 0;
   for (let i = 0; i < list.length; i++) {
-    if (search_element === list[i]) {
+    if (searchElement === list[i]) {
       c++;
     }
   }
