4ce10fb512f2b95cdaa147ea65946abf1621f7bf
meghamohan <meghamohan1989@gmail.com>
2017-09-26 04:25:20 +0000

    trying to fix iisue

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // square inherits from rectangle
-const rectangle = require('./4-rectangle').Rectangle;
+const Rectangle = require('./4-rectangle').Rectangle;
 // define square function
 function Square (size) {
-  rectangle.call(this, size, size);
+  Rectangle.call(this, size, size);
 }
 // export the square function
 exports.Square = Square;
